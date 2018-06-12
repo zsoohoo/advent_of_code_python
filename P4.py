2b18431072a9b53edc12e63394340d06118cd28e
zsoohoo <zach.soohoo@gmail.com>
2018-06-12 14:27:37 -0700

    Finished P4 and complete P5

--- P4.py
+++ P4.py
@@ -1,10 +1,29 @@
+from collections import Counter
+
+def countWords(words):
+	for i in range(len(words)):
+		if words.count(words[i])>1:
+			return False
+		if i == len(words)-1:
+			return True
+
+def countAnagrams(words):
+	letterCounts = []
+	for i in range(len(words)):
+		letterNum =  Counter(words[i])
+		if letterNum in letterCounts:
+			return False
+		letterCounts.append(letterNum)
+	return True
+
 with open('P4Input.txt') as f:
-	count = 0
+	nonRepeat = 0
+	nonAnagram =0
 	for line in f:
 		words = line.split()
-		for i in range(len(words)):
-			if words.count(words[i])>1:
-				break
-			if i == len(words)-1:
-				count += 1
-	print('{} valid passphrases in the file'.format(count))
+		if countWords(words) == True:
+			nonRepeat +=1
+		if countAnagrams(words) == True:
+			nonAnagram +=1
+	print('Problem 4 Part 1 solution is: {}'.format(nonRepeat))
+	print('Problem 4 Part 2 solution is: {}'.format(nonAnagram))
---
+++ P5.py
@@ -0,0 +1,28 @@
+def stepThrough(commands, mode):
+	offset = [0] * len(commands)
+	current = 0
+	iterations = 0
+	highest = 0
+	if mode == 0:
+		while current < len(commands):
+			move = commands[current] + offset[current]
+			offset[current] += 1
+			current += move
+			iterations += 1
+		return iterations
+	else:
+		while current < len(commands):
+			move = commands[current] + offset[current]
+			if move > 2:
+				offset[current] -= 1
+			else:
+				offset[current] += 1
+			current += move
+			iterations += 1
+		return iterations
+
+with open('P5Input.txt') as f:
+	commands = [int(line.rstrip()) for line in f]
+	#commands = [0,3,0,1,-3]
+	print('Problem 2 Part 1 solution is: {}'.format(stepThrough(commands, 0)))
+	print('Problem 2 Part 2 solution is: {}'.format(stepThrough(commands, 1)))
